9d18c37bed4ed4048fa49f3811036079b7ec96f8
Menghaoliu77 <ireneliu@my.yorku.ca>
2022-04-12 04:07:24 -0400

    1

--- app.py
+++ app.py
@@ -1,35 +1,25 @@
-import webbrowser
-# open form.html file in write-only mode
-# it will automatically create the file
-# if it is not exist
-file = open("form.html", "w")
-# write the html code for registration form 
-html_code = """<!DOCTYPE html>
-<html>
-<head>
-<title>Registration Form</title>
-</head>
-<body>
-<h1>Registration Form</h1>
-<form action="/action_page.php">
-<label for="name"> <b>Enter your Name:</b> </label>
-<input type="text" id="name" name="name"> <br><br>
-<label for="email"> <b>Enter your Email:</b> </label> 
-<input type="email" id="email" name="email"> <br>
-<h3>Select Course:</h3>
-<input type="radio" id="Course1" name="Course" value="C++">
-<label for="Course1"> <b> C++ </b> </label> <br>
-<input type="radio" id="Course2" name="Course" value="Java">
-<label for="Course2"> <b> Java </b> </label> <br>
-<input type="radio" id="Course2" name="Course" value="Python">
-<label for="Course3"> <b> Python </b> </label> <br><br>
-<input type="submit" value="Submit">
-</form>
-</body>
-</html>"""
-# write the html code in form.html file
-file.write(html_code)
-# close the file
-file.close()
-#open the form in new tab of browser
-webbrowser.open_new_tab("form.html")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+
+    # Load current count
+    f = open("count.txt", "r")
+    count = int(f.read())
+    f.close()
+
+    # Increment the count
+    count += 1
+
+    # Overwrite the count
+    f = open("count.txt", "w")
+    f.write(str(count))
+    f.close()
+
+    # Render HTML with count variable
+    return render_template("index.html", count=count)
+
+if __name__ == "__main__":
+    app.run()
